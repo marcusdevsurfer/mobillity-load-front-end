9679d4488942c71517d91b50f7eaffff3ea512ad
marcusdev99 <marcus.sea13@gmail.com>
2023-09-14 17:40:09 -0600

    feat : addition of SearchBar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const SearchBar = () => {
+    return (
+        <div className='container-fluid my-2'>
+            <div className='row'>
+                <div className='col-sm-12 d-flex justify-content-center'>
+                    <form action="">
+                        <input type="date" />
+                        <button type='submit' className='btn btn-primary p-1'>Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}

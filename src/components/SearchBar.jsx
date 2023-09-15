5263c83710a4401056adb642527edd4ff5935ba4
marcusdev99 <marcus.sea13@gmail.com>
2023-09-14 22:43:06 -0600

    refactor : aggregation of flex box to form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ export const SearchBar = () => {
         <div className='container-fluid my-2'>
             <div className='row'>
                 <div className='col-sm-12 d-flex justify-content-center'>
-                    <form action="">
+                    <form className='d-flex' action="">
                         <input type="date" />
                         <button type='submit' className='btn btn-primary p-1'>Buscar</button>
                     </form>

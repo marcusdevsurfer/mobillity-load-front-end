8ce6360c67bf44989fc2e5d056672fc3fac2b9e6
marcusdev99 <marcus.sea13@gmail.com>
2023-09-26 15:49:35 -0600

    feature : addition of routes to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { AddUnitForm } from "./components/AddUnitForm";
+import { CarrierList } from "./components/CarrierList";
+import { CarrierProfile } from "./components/CarrierProfile";
 import { ListOfUnits } from "./components/ListOfUnits";
+import { LoginForm } from "./components/LoginForm";
 import { Navbar } from "./components/Navbar";
+import { RegisterForm } from "./components/RegisterForm";
 import { SearchBar } from "./components/SearchBar";
+
+import { Route, Routes } from "react-router-dom";
 
 
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar/>
-      <SearchBar />
-      <ListOfUnits />
-    </>
+      <Routes>
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/search-page" element={<CarrierList />} />
+      </Routes>
+
+    </div>
+
   );
 }
 
